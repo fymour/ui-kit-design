51676e078d1a7584cd84726274bcfc1a71112391
Sulaimanova Nadira <nadira18.sulaimanova@gmail.com>
2022-07-20 16:50:09 +0600

    add button

---
+++ src/Components/Pages/ButtonPage/ButtonPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from 'ui-kit/Button/Button'
+
+export const ButtonPage = () => {
+  return (
+    <div>
+        <Button children={"Button"} disableStyle={''} hoverStyle={''} activeStyle={''}/>
+        <Button children={"Button"} disabled={true} disableStyle={''} hoverStyle={''} activeStyle={''}/>
+    </div>
+  )
+}
--- src/Components/Pages/Pages.tsx
+++ src/Components/Pages/Pages.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Introduction from "./Introduction";
 import './Pages.css'
+import { ButtonPage } from './ButtonPage/ButtonPage';
 
 export default function Pages() {
   return (
     <div className='pages'>
         <Routes>
           <Route path="/introduction" element={<Introduction/>}/>
+          <Route path="/button" element={<ButtonPage/>}/> 
         </Routes>
     </div>
   )
