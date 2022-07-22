412c3e0687d5cb4c759e504ec4337071b411427b
Sulaimanova Nadira <nadira18.sulaimanova@gmail.com>
2022-07-22 17:19:23 +0600

    add glossary

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -5,12 +5,13 @@ import './Menu.css'
 export default function Menu() {
   return (
     <div className='menu'>
-        <Link to="/introduction"><div>Введение</div></Link>
+        <Link to="/"><div>Введение</div></Link>
+        <Link to="/glossary"><div>Глоссарий</div></Link>
         <Link to="/button"><div>Button</div></Link>
         <Link to="/input"><div>Input</div></Link>
         <Link to="/tabs"><div>Tabs</div></Link>
         <Link to="/collapse"><div>Collapse</div></Link>
-        <Link to="/dropdownMenu"><div>DropdownMenu</div></Link>
+        <Link to="/footer"><div>Footer</div></Link>
         <Link to="/scrollArea"><div>ScrollArea</div></Link>
     </div>
   )
--- src/Components/Pages/Pages.tsx
+++ src/Components/Pages/Pages.tsx
@@ -7,15 +7,19 @@ import CollapsePage from './CollapsePage/CollapsePage';
 import InputPage from './InputPage/InputPage';
 import ScrollAreaPage from './ScrollAreaPage/ScrollAreaPage';
 import TabsPage from './TabsPage/TabsPage';
+import { FooterPage } from './FooterPage/FooterPage';
+import { Glossary } from './Glossary/Glossary';
 
 export default function Pages() {
   return (
     <div className='pages'>
         <Routes>
-          <Route path="/introduction" element={<Introduction/>}/>
+          <Route path="/" element={<Introduction/>}/>
+          <Route path="/glossary" element={<Glossary/>}/> 
           <Route path="/button" element={<ButtonPage/>}/> 
           <Route path="/collapse" element={<CollapsePage/>}/> 
           <Route path="/input" element={<InputPage/>}/> 
+          <Route path="/footer" element={<FooterPage/>}/> 
           <Route path="/scrollarea" element={<ScrollAreaPage/>}/> 
           <Route path="/tabs" element={<TabsPage/>}/> 
         </Routes>
