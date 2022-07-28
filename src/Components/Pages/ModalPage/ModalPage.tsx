0682c12cb9d6480edf20a378489e818af5880960
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2022-07-28 14:26:39 +0600

    add modal

--- src/Components/Pages/ModalPage/ModalPage.tsx
+++ src/Components/Pages/ModalPage/ModalPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Modal } from "ui-kit";
+import iconColendar from '../../Assets/image/small.svg'
 import "./ModalPage.scss";
 
 export const ModalPage: React.FC = () => {
@@ -21,14 +22,25 @@ export const ModalPage: React.FC = () => {
     <section className="ModalPage">
       <h2>Modal</h2>
       <Button onClick={handleModalOpen} disableStyle={""} hoverStyle={""} activeStyle={""}>Open</Button>
+      
       <Modal isOpen={isOpenModal} onCloseModal={handleModalClose}>
         <Modal.Header>
-          <h2>Title</h2>
+          <h2>12 Августа, 11:00</h2>
         </Modal.Header>
         <Modal.Content>
-          <div>Content</div>
+          <div className="flex">
+            <div className="img"> 
+              <img src={iconColendar} alt="" />
+            </div>
+            <div className="collum">
+              <h3>Инфографика</h3>
+              <p>Представление информации и данных. Создание презентаций</p>
+              <div><p>Лектор: Александр Сергеевич</p></div>
+              <div><p>Кабинет: 302</p></div>
+            </div>
+          </div>
         </Modal.Content>
-        <Modal.Footer buttonSubmitText="Choice" onSubmit={handleModalSubmit} />
+        {/* <Modal.Footer buttonSubmitText="close" onSubmit={handleModalSubmit} /> */}
       </Modal>
     </section>
   );
