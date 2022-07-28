70e3e2505a602245276d01571fbd59a1527ae754
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-07-28 17:51:01 +0600

    typograph

--- src/ui-kit/Checkbox/Checkbox.tsx
+++ src/ui-kit/Checkbox/Checkbox.tsx
@@ -1,37 +1,66 @@
 import React from 'react'
 import classNames from 'classnames';
+import {Icon} from '../../ui-kit'
 import './Checkbox.scss'
 
 interface CheckboxProps {
-    className?:string
+    // className?:string
+    variant?: 'deselected' | 'selected' | 'mixed'
+    size?: 'small' | 'medium' | 'large';
     id: string 
+    hover?: boolean
+    hoverStyle: string
+    active?: boolean
+    activeStyle: string
     label?:string
     isChecked: boolean
     onChange?:(event:React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export const Checkbox: React.FC<CheckboxProps> = ({
-    className, 
+    // className, 
+    variant = 'deselected',
+    size = 'small',
     id, 
+    hover =false,
+    hoverStyle,
+    active = true,
+    activeStyle,
     label, 
     isChecked, 
     onChange}) =>{
+        
+        if(hover){
+            var hoverStyle = 'hover'
+        }
+        if(active){
+            var activeStyle = 'active'
+        }
+
     return(
         <label
-        className={classNames('Checkbox',className,{
+        className={classNames('Checkbox',{
             Checkbox_active: isChecked,
         })}
         htmlFor={id}
         >
+            <input className={
+                [`checkbox`,
+                `checkbox--${size}`,
+                `checkbox--${variant}`,
+                `checkbox--${hoverStyle}-${variant}`,
+                `checkbox--${activeStyle}-${variant}`
+             ].join(' ')
+            } type="checkbox"
+            checked={isChecked}
+            id={id}
+            name={label}
+            value={label}
+            onChange={onChange}
+             />
             <span
-            className='checkbox-inner'>
-                <input className='checkbox-icon' type="checkbox"
-                checked={isChecked}
-                id={id}
-                name={label}
-                value={label}
-                onChange={onChange}
-                 />
+            className='checkbox-span'>
+                <Icon className='checkbox-icon' type={'Checkbox'} />
             </span>
         </label>
     )
